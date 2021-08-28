9cbfe88150a85f613a9f79462ff32b94b02c90ee
Dawie <dvanlill@sun.ac.za>
2021-08-28 23:42:01 +0200

    Setting up the topics

---
+++ tut1-introduction.jl
@@ -0,0 +1,56 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ ea80a138-0847-11ec-3f65-ed1ea62e1f73
+html"""
+<div style="
+position: absolute;
+width: calc(100% - 30px);
+border: 50vw solid #2e3440;
+border-top: 200px solid #2e3440;
+border-bottom: none;
+box-sizing: content-box;
+left: calc(-50vw + 15px);
+top: -100px;
+height: 100px;
+pointer-events: none;
+"></div>
+
+<div style="
+height: 200px;
+width: 100%;
+background: #2e3440;
+color: #fff;
+padding-top: 50px;
+">
+<span style="
+font-family: Vollkorn, serif;
+font-weight: 700;
+font-feature-settings: 'lnum', 'pnum';
+"> <p style="
+font-size: 1.5rem;
+opacity: 0.8;
+">Macro 318</p>
+<p style="text-align: center; font-size: 1.8rem;">
+ Julia for aspiring economists
+</p>
+
+<style>
+body {
+overflow-x: hidden;
+}
+</style>"""
+
+# ╔═╡ ab0e1d94-5f52-4f33-bcc3-51a60cb0cec5
+md""" # Introduction """
+
+# ╔═╡ f3d6382d-418e-472a-bfc7-f7d0a67fc842
+md""" In this notebook we will give a broad overview of the Julia programming language and how it can be useful for you as an aspiring economist. Do not be too concerned about the language and syntax, most of you are new to the idea of programming and will find this too be potentially scary at first. """
+
+# ╔═╡ Cell order:
+# ╟─ea80a138-0847-11ec-3f65-ed1ea62e1f73
+# ╟─ab0e1d94-5f52-4f33-bcc3-51a60cb0cec5
+# ╟─f3d6382d-418e-472a-bfc7-f7d0a67fc842
---
+++ tut2-solow.jl
@@ -0,0 +1,52 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ b12f975e-0847-11ec-3a9d-4978b3b7979b
+html"""
+<div style="
+position: absolute;
+width: calc(100% - 30px);
+border: 50vw solid #2e3440;
+border-top: 200px solid #2e3440;
+border-bottom: none;
+box-sizing: content-box;
+left: calc(-50vw + 15px);
+top: -100px;
+height: 100px;
+pointer-events: none;
+"></div>
+
+<div style="
+height: 200px;
+width: 100%;
+background: #2e3440;
+color: #fff;
+padding-top: 50px;
+">
+<span style="
+font-family: Vollkorn, serif;
+font-weight: 700;
+font-feature-settings: 'lnum', 'pnum';
+"> <p style="
+font-size: 1.5rem;
+opacity: 0.8;
+">Macro 318</p>
+<p style="text-align: center; font-size: 1.8rem;">
+ The Solow model
+</p>
+
+<style>
+body {
+overflow-x: hidden;
+}
+</style>"""
+
+# ╔═╡ f131c8e4-89c9-4899-9150-dbca5f5e6c90
+md""" In this section we will write some code that will solve the Solow model. This will help you to see that we can actually use computers to solve some of the models that are being discussed during lectures. """
+
+# ╔═╡ Cell order:
+# ╟─b12f975e-0847-11ec-3a9d-4978b3b7979b
+# ╟─f131c8e4-89c9-4899-9150-dbca5f5e6c90
